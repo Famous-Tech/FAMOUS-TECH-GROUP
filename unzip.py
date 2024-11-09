b4aafed1ddad9d4bab9d7e7800790b9499ed2457
FAMOUS-TECH <167099698+Famous-Tech@users.noreply.github.com>
2024-11-09 13:10:49 -0500

    Update unzip.py

--- unzip.py
+++ unzip.py
@@ -3,7 +3,7 @@ import os
 import shutil
 
 # Nom du fichier ZIP et répertoire de sortie
-zip_file_path = "FAMOUS-TECH-GROUP-main_125316.zip"  # Nom de votre fichier ZIP
+zip_file_path = "./FAMOUS-TECH-GROUP-main_125316.zip"  # Nom de votre fichier ZIP
 output_dir = "."  # Répertoire de sortie, ici la base du dépôt
 
 # Liste des fichiers à conserver (ici uniquement ce script)
